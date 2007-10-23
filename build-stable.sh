134569a6547b2d4941015a7a090022bb1e8143d5
duz <duz@9d9971a0-073c-0410-b583-986eaf280b9e>
2007-10-23 06:08:19 +0000

    Rearranging the file names for easier seperation of wip and stable
    git-svn-id: https://nzbdstatus.svn.sourceforge.net/svnroot/nzbdstatus/trunk@18 9d9971a0-073c-0410-b583-986eaf280b9e

--- build-stable.sh
+++ build-stable.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+mv install-stable.rdf install.rdf
 7z a -tzip nzbdstatus-stable.xpi * -r -mx=9 -xr\!.svn -x\!\*.bat -x\!\*.sh -x\!\*.xpi
+mv install.rdf install-stable.rdf
--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+mv install-wip.rdf install.rdf
 7z a -tzip nzbdstatus.xpi * -r -mx=9 -xr\!.svn -x\!\*.bat -x\!\*.sh -x\!\*.xpi
+mv install.rdf install-wip.rdf
