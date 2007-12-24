11a42b2a701dda09edd86bc5770f0adba20519d3
duz <duz@9d9971a0-073c-0410-b583-986eaf280b9e>
2007-12-24 07:34:26 +0000

    Adding options for legacy mode and file send Adding JS to disable theme selector on legacy mode dependancy
    git-svn-id: https://nzbdstatus.svn.sourceforge.net/svnroot/nzbdstatus/trunk@41 9d9971a0-073c-0410-b583-986eaf280b9e

--- chrome/content/configuration.js
+++ chrome/content/configuration.js
@@ -1,9 +1,18 @@
 function prefonload()
 {
+	// See if template selector should be enabled
+	document.getElementById('sabtheme').disabled = !(document.getElementById('sablegacy').checked);
+
+	// Check to see if we're supposed to open a certain panel
 	if (window.arguments.length == 0)
 	{
 		return;
 	}
 	var selected = document.getElementById(window.arguments[0]);
 	prefwindow.showPane(selected);
+}
+
+function legacyClicked()
+{
+	document.getElementById('sabtheme').disabled = document.getElementById('sablegacy').checked;
 }
