0101001c2396e733616dafecdadc1b0bdb66485c
duz <duz@9d9971a0-073c-0410-b583-986eaf280b9e>
2008-05-09 03:13:49 +0000

    Renamed preferences branch
    git-svn-id: https://nzbdstatus.svn.sourceforge.net/svnroot/nzbdstatus/trunk@94 9d9971a0-073c-0410-b583-986eaf280b9e

--- defaults/preferences/sabnzbdstatus.js
+++ defaults/preferences/sabnzbdstatus.js
@@ -1,32 +1,64 @@
-pref('extensions.sabnzbdstatus.disable', false);
-pref('extensions.sabnzbdstatus.refreshRate', 5);
-pref('extensions.sabnzbdstatus.alwaysShowIcon', true);
-pref('extensions.sabnzbdstatus.onlyShowIcon', false);
-pref('extensions.sabnzbdstatus.leftClick', 'refresh');
-pref('extensions.sabnzbdstatus.middleClick', 'newtab');
+pref('extensions.nzbdstatus.disable', false);
+pref('extensions.nzbdstatus.refreshRate', 5);
+pref('extensions.nzbdstatus.alwaysShowIcon', true);
+pref('extensions.nzbdstatus.onlyShowIcon', false);
+pref('extensions.nzbdstatus.leftClick', 'refresh');
+pref('extensions.nzbdstatus.middleClick', 'newtab');
 
-pref('extensions.sabnzbdstatus.iconIdle', 'chrome://sabnzbdstatus/skin/sab.png');
-pref('extensions.sabnzbdstatus.iconDownload', 'chrome://sabnzbdstatus/skin/download.png');
-pref('extensions.sabnzbdstatus.iconPaused', 'chrome://sabnzbdstatus/skin/pause.png');
+pref('extensions.nzbdstatus.iconIdle', 'chrome://sabnzbdstatus/skin/sab.png');
+pref('extensions.nzbdstatus.iconDownload', 'chrome://sabnzbdstatus/skin/download.png');
+pref('extensions.nzbdstatus.iconPaused', 'chrome://sabnzbdstatus/skin/pause.png');
 
-pref('extensions.sabnzbdstatus.sabUrl', 'http://localhost:8080/sabnzbd/');
-pref('extensions.sabnzbdstatus.queueUrl', 'api?mode=qstatus&output=json');
-pref('extensions.sabnzbdstatus.pauseUrl', 'api?mode=pause');
-pref('extensions.sabnzbdstatus.unpauseUrl', 'api?mode=resume');
-pref('extensions.sabnzbdstatus.addUrl', 'api');
-pref('extensions.sabnzbdstatus.addID', 'api');
-pref('extensions.sabnzbdstatus.addFile', 'api');
+pref('extensions.nzbdstatus.sabUrl', 'http://localhost:8080/sabnzbd/');
+pref('extensions.nzbdstatus.queueUrl', 'api?mode=qstatus&output=json');
+pref('extensions.nzbdstatus.pauseUrl', 'api?mode=pause');
+pref('extensions.nzbdstatus.unpauseUrl', 'api?mode=resume');
+pref('extensions.nzbdstatus.historyUrl', 'rss?mode=history');
+pref('extensions.nzbdstatus.addUrl', 'api');
+pref('extensions.nzbdstatus.addID', 'api');
+pref('extensions.nzbdstatus.addFile', 'api');
 
-pref('extensions.sabnzbdstatus.legacyMode', false);
+pref('extensions.nzbdstatus.legacyMode', false);
 
-pref('extensions.sabnzbdstatus.template', 'None');
-pref('extensions.sabnzbdstatus.sabusername', '');
-pref('extensions.sabnzbdstatus.sabpassword', '');
+pref('extensions.nzbdstatus.template', 'None');
+pref('extensions.nzbdstatus.sabusername', '');
+pref('extensions.nzbdstatus.sabpassword', '');
 
-pref('extensions.sabnzbdstatus.enableInNewzbin', true);
-pref('extensions.sabnzbdstatus.newzbinToSAB', 3);
+pref('extensions.nzbdstatus.enableInNewzbin', true);
+pref('extensions.nzbdstatus.newzbinToSAB', 3);
 
-pref('extensions.sabnzbdstatus.enableFilesToSAB', true);
-pref('extensions.sabnzbdstatus.filesToSAB', 3);
+pref('extensions.nzbdstatus.enableFilesToSAB', true);
+pref('extensions.nzbdstatus.filesToSAB', 3);
 
-pref('extensions.sabnzbdstatus.editorMode', false);
+pref('extensions.nzbdstatus.editorMode', false);
+
+pref('extensions.nzbdstatus.servers.count', 6);
+pref('extensions.nzbdstatus.servers.1.url', 'http://192.168.1.100:8080/sabnzbd/');
+pref('extensions.nzbdstatus.servers.1.label', 'Home');
+pref('extensions.nzbdstatus.servers.1.type', 'sabnzbd+');
+pref('extensions.nzbdstatus.servers.1.color', 'yellow');
+
+pref('extensions.nzbdstatus.servers.2.url', 'http://dq5.no-ip.info:8080/sabnzbd/');
+pref('extensions.nzbdstatus.servers.2.label', 'Work');
+pref('extensions.nzbdstatus.servers.2.type', 'sabnzbd+');
+pref('extensions.nzbdstatus.servers.2.color', 'yellow');
+
+pref('extensions.nzbdstatus.servers.3.url', 'http://192.168.1.100:8080/sabnzbd/');
+pref('extensions.nzbdstatus.servers.3.label', 'NAS');
+pref('extensions.nzbdstatus.servers.3.type', 'sabnzbd+');
+pref('extensions.nzbdstatus.servers.3.color', 'yellow');
+
+pref('extensions.nzbdstatus.servers.4.url', 'http://dq5.no-ip.info:8080/sabnzbd/');
+pref('extensions.nzbdstatus.servers.4.label', 'Test');
+pref('extensions.nzbdstatus.servers.4.type', 'sabnzbd+');
+pref('extensions.nzbdstatus.servers.4.color', 'yellow');
+
+pref('extensions.nzbdstatus.servers.5.url', 'http://192.168.1.100:8080/sabnzbd/');
+pref('extensions.nzbdstatus.servers.5.label', 'Server 1');
+pref('extensions.nzbdstatus.servers.5.type', 'sabnzbd+');
+pref('extensions.nzbdstatus.servers.5.color', 'yellow');
+
+pref('extensions.nzbdstatus.servers.6.url', 'http://dq5.no-ip.info:8080/sabnzbd/');
+pref('extensions.nzbdstatus.servers.6.label', 'Server 2');
+pref('extensions.nzbdstatus.servers.6.type', 'sabnzbd+');
+pref('extensions.nzbdstatus.servers.6.color', 'yellow');
