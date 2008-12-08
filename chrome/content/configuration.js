f1577e0c02e4f5f7bfcb8c96b35c3261ce1abc93
duz <duz@9d9971a0-073c-0410-b583-986eaf280b9e>
2008-12-08 23:43:55 +0000

    Complete rewrite to an object
    git-svn-id: https://nzbdstatus.svn.sourceforge.net/svnroot/nzbdstatus/trunk@180 9d9971a0-073c-0410-b583-986eaf280b9e

--- chrome/content/configuration.js
+++ chrome/content/configuration.js
@@ -13,59 +13,198 @@
 //      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 //      MA 02110-1301, USA.
 
-// Originally based off inpheaux's SABnzbd/Newzbin Greasemonkey script, has deviated a bit since then
-// (c) 2008 Ben Dusinberre
 
-function prefonload()
+function nzbdStatusConfigObject()
 {
-	// See if template selector should be enabled
-	document.getElementById('sabtheme').disabled = !(document.getElementById('sablegacy').checked);
+}
+// Now extend it
+nzbdStatusConfigObject.prototype = {
 
-	// Check to see if we're supposed to open a certain panel
-	if (window.arguments.length == 0)
+	// Private variables
+	_preferences: Components.classes['@mozilla.org/preferences;1']
+	 .getService(Components.interfaces.nsIPrefService)
+	 .getBranch('extensions.nzbdstatus.'),
+
+	// Getters
+
+	// Our link to the preference service
+	get preferences()
 	{
-		return;
+		return this._preferences;
+	},
+
+
+	// Functions
+
+	// Returns the value at the given preference from the branch in the preference property
+	// @param: (string) Preference name
+	// @return: (boolean, string or int) Preference value or NULL if not found
+	getPreference: function(prefName)
+	{
+		var prefValue, prefType = this.preferences.getPrefType(prefName);
+		switch (prefType)
+		{
+			case this.preferences.PREF_BOOL:
+				prefValue = this.preferences.getBoolPref(prefName);
+				break;
+			case this.preferences.PREF_INT:
+				prefValue = this.preferences.getIntPref(prefName);
+				break;
+			case this.preferences.PREF_STRING:
+				prefValue = this.preferences.getCharPref(prefName);
+				break;
+			case this.preferences.PREF_INVALID:
+			default:
+				prefValue = null;
+		}
+		return prefValue;
+	},
+
+	// Set the given preference to the given value in the branch in the preference property
+	// @param: (string) Preference name, (boolean, string or int) Preference value
+	// @return: (boolean) Success in updating preference
+	setPreference: function(prefName, prefValue)
+	{
+		var success = true, prefType = this.preferences.getPrefType(prefName);
+		switch (prefType)
+		{
+			case this.preferences.PREF_BOOL:
+				prefValue = this.preferences.setBoolPref(prefName, prefValue);
+				break;
+			case this.preferences.PREF_INT:
+				prefValue = this.preferences.setIntPref(prefName, prefValue);
+				break;
+			case this.preferences.PREF_STRING:
+				prefValue = this.preferences.setCharPref(prefName, prefValue);
+				break;
+			case this.preferences.PREF_INVALID:
+			default:
+				success = false;
+		}
+		return success;
+	},
+
+	prefonload: function()
+	{
+		try {
+		nzbdStatusConfig.buildServerList();
+		nzbdStatusConfig.buildServerDeck();
+		nzbdStatusConfig.buildServerPreferences();
+		// Check to see if we're supposed to open a certain panel
+		if (window.arguments && window.arguments.length != 0)
+		{
+			var selected = document.getElementById(window.arguments[0]);
+			document.getElementById('nzbd-prefs').showPane(selected);
+		}
+		window.sizeToContent();
+		} catch(e) {dump('prefonload error: '+e+'\n'); }
+	},
+
+	buildServerList: function()
+	{
+		try {
+		var sList = document.getElementById('nzbdserver-list');
+		var serverCount = this.getPreference('servers.count');
+		var newListItem;
+		for (i = 0; i < serverCount; i++)
+		{
+			newListItem = document.createElement('listitem');
+			newListItem.setAttribute('label', this.getPreference('servers.'+i+'.label'));
+			sList.appendChild(newListItem);
+		}
+		} catch(e) {dump('buildlists error: '+e+'\n'); }
+	},
+
+	buildServerDeck: function()
+	{
+		var sDeck = document.getElementById('nzbdserver-deck');
+		var sTemplate = document.getElementById('nzbdserver-deck-template');
+		var sPrefs = document.getElementById('nzbdserver-preferences');
+		var serverCount = this.getPreference('servers.count');
+		var newDeck, elems, nAtt;
+		for (i = 0; i < serverCount; i++)
+		{
+			newDeck = sTemplate.cloneNode(true);
+			newDeck.getElementsByTagName('caption')[0].setAttribute('label', this.getPreference('servers.'+i+'.label'));
+			elems = newDeck.getElementsByClassName('nzbd-updatepref');
+			for (j = 0; j < elems.length; j++)
+			{
+				nAtt = elems[j].getAttribute('preference');
+				elems[j].setAttribute('preference', nAtt.replace(/template/, i));
+			}
+			elems = newDeck.getElementsByTagName('radio');
+			elems[0].setAttribute('value', i);
+			sDeck.insertBefore(newDeck, sTemplate);
+		}
+	},
+
+	buildServerPreferences: function()
+	{
+		var sPrefs = document.getElementById('nzbdserver-preferences');
+		var serverCount = this.getPreference('servers.count');
+		var newPref;
+		for (i = 0; i < serverCount; i++)
+		{
+			newPref = document.createElement('preference');
+			newPref.setAttribute('id', 'nzbdlabel-'+i);
+			newPref.setAttribute('name', 'extensions.nzbdstatus.servers.'+i+'.label');
+			newPref.setAttribute('type', 'string');
+			sPrefs.appendChild(newPref);
+			newPref = document.createElement('preference');
+			newPref.setAttribute('id', 'nzbdurl-'+i);
+			newPref.setAttribute('name', 'extensions.nzbdstatus.servers.'+i+'.url');
+			newPref.setAttribute('type', 'string');
+			sPrefs.appendChild(newPref);
+			newPref = document.createElement('preference');
+			newPref.setAttribute('id', 'nzbdserver-'+i);
+			newPref.setAttribute('name', 'extensions.nzbdstatus.servers.'+i+'.type');
+			newPref.setAttribute('type', 'string');
+			sPrefs.appendChild(newPref);
+			newPref = document.createElement('preference');
+			newPref.setAttribute('id', 'nzbdicon-'+i);
+			newPref.setAttribute('name', 'extensions.nzbdstatus.servers.'+i+'.icon');
+			newPref.setAttribute('type', 'string');
+			sPrefs.appendChild(newPref);
+			newPref = document.createElement('preference');
+			newPref.setAttribute('id', 'nzbdshownotif-'+i);
+			newPref.setAttribute('name', 'extensions.nzbdstatus.servers.'+i+'.showNotifications');
+			newPref.setAttribute('type', 'bool');
+			sPrefs.appendChild(newPref);
+			newPref = document.createElement('preference');
+			newPref.setAttribute('id', 'nzbdenable-'+i);
+			newPref.setAttribute('name', 'extensions.nzbdstatus.servers.'+i+'.enable');
+			newPref.setAttribute('type', 'bool');
+			sPrefs.appendChild(newPref);
+			newPref = document.createElement('preference');
+			newPref.setAttribute('id', 'nzbdshowstatus-'+i);
+			newPref.setAttribute('name', 'extensions.nzbdstatus.servers.'+i+'.showInStatusBar');
+			newPref.setAttribute('type', 'bool');
+			sPrefs.appendChild(newPref);
+		}
+
+	},
+
+	changeServerDeck: function()
+	{
+		try {
+		var switchTo = document.getElementById('nzbdserver-list').currentIndex;
+		document.getElementById('nzbdserver-deck').selectedIndex = switchTo;
+		} catch(e) {dump('prefonload error: '+e+'\n'); }
 	}
-	var selected = document.getElementById(window.arguments[0]);
-	prefwindow.showPane(selected);
+
 }
 
-function legacyClicked()
-{
-	document.getElementById('sabtheme').disabled = document.getElementById('sablegacy').checked;
-}
+nzbdStatusConfig = new nzbdStatusConfigObject();
+
 
 function ensureEndSlash()
 {
 	var _preferences = Components.classes['@mozilla.org/preferences;1']
 	 .getService(Components.interfaces.nsIPrefService)
-	 .getBranch('extensions.sabnzbdstatus.');
+	 .getBranch('extensions.nzbdstatus.');
 	var sabUrl = _preferences.getCharPref('sabUrl');
 	if (sabUrl[sabUrl.length-1] != '/')
 	{
 		_preferences.setCharPref('sabUrl', sabUrl + '/');
 	}
-}
-
-function preferences()
-{
-	return Components.classes['@mozilla.org/preferences;1']
-	 .getService(Components.interfaces.nsIPrefService)
-	 .getBranch('extensions.nzbdstatus.');
-}
-
-function addServer()
-{
-	var serverCount = preferences().getIntPref('servers.count');
-	var newServer = document.getElementById('server'+serverCount).cloneNode(true);
-	serverCount++;
-	newServer.id = 'server' + serverCount;
-	var elems = newServer.getElementsByTagName('textbox');
-	elems[0].setAttribute('preference', elems[0].getAttribute('preference').replace(serverCount-1, serverCount));
-	elems[1].setAttribute('preference', elems[1].getAttribute('preference').replace(serverCount-1, serverCount));
-	var elems = newServer.getElementsByTagName('menulist');
-	elems[0].setAttribute('preference', elems[0].getAttribute('preference').replace(serverCount-1, serverCount));
-	elems[1].setAttribute('preference', elems[1].getAttribute('preference').replace(serverCount-1, serverCount));
-	document.getElementById('nzbd-servers').appendChild(newServer);
-	window.sizeToContent();
 }
