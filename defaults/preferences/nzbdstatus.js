1f3079424c7fb5f82d8de315cde7f01a0b79f5a2
duz <duz@9d9971a0-073c-0410-b583-986eaf280b9e>
2008-12-08 23:31:54 +0000

    Fix timeoutSecs preference to use seconds Add enable, showNotifications, and showInStatusBar preferences for each server
    git-svn-id: https://nzbdstatus.svn.sourceforge.net/svnroot/nzbdstatus/trunk@176 9d9971a0-073c-0410-b583-986eaf280b9e

--- defaults/preferences/nzbdstatus.js
+++ defaults/preferences/nzbdstatus.js
@@ -30,29 +30,44 @@ pref('extensions.nzbdstatus.servers.count', 5);
 pref('extensions.nzbdstatus.servers.favorite', 0);
 
 pref('extensions.nzbdstatus.servers.retryLimit', 3);
-pref('extensions.nzbdstatus.servers.timeoutSecs', 15000);
+pref('extensions.nzbdstatus.servers.timeoutSecs', 15);
 
-pref('extensions.nzbdstatus.servers.0.url', 'http://localhost:8080/sabnzbd/');
+pref('extensions.nzbdstatus.servers.0.enable', true);
+pref('extensions.nzbdstatus.servers.0.showNotifications', true);
+pref('extensions.nzbdstatus.servers.0.showInStatusBar', true);
 pref('extensions.nzbdstatus.servers.0.label', 'Home');
+pref('extensions.nzbdstatus.servers.0.url', 'http://localhost:8080/sabnzbd/');
 pref('extensions.nzbdstatus.servers.0.type', 'sabnzbd+');
 pref('extensions.nzbdstatus.servers.0.icon', 'yellow.png');
 
-pref('extensions.nzbdstatus.servers.1.url', 'http://localhost:8080/sabnzbd/');
+pref('extensions.nzbdstatus.servers.1.enable', true);
+pref('extensions.nzbdstatus.servers.1.showNotifications', true);
+pref('extensions.nzbdstatus.servers.1.showInStatusBar', true);
 pref('extensions.nzbdstatus.servers.1.label', 'Copy of Home');
+pref('extensions.nzbdstatus.servers.1.url', 'http://localhost:8080/sabnzbd/');
 pref('extensions.nzbdstatus.servers.1.type', 'sabnzbd+');
 pref('extensions.nzbdstatus.servers.1.icon', 'flag_blue.png');
 
-pref('extensions.nzbdstatus.servers.2.url', 'http://dq5.no-ip.info:8080/sabnzbd/');
+pref('extensions.nzbdstatus.servers.2.enable', true);
+pref('extensions.nzbdstatus.servers.2.showNotifications', true);
+pref('extensions.nzbdstatus.servers.2.showInStatusBar', true);
 pref('extensions.nzbdstatus.servers.2.label', 'Work');
+pref('extensions.nzbdstatus.servers.2.url', 'http://dq5.no-ip.info:8080/sabnzbd/');
 pref('extensions.nzbdstatus.servers.2.type', 'sabnzbd+');
 pref('extensions.nzbdstatus.servers.2.icon', 'flag_red.png');
 
-pref('extensions.nzbdstatus.servers.3.url', 'http://localhost:8080/sabnzbd/');
+pref('extensions.nzbdstatus.servers.3.enable', true);
+pref('extensions.nzbdstatus.servers.3.showNotifications', true);
+pref('extensions.nzbdstatus.servers.3.showInStatusBar', true);
 pref('extensions.nzbdstatus.servers.3.label', 'NAS');
+pref('extensions.nzbdstatus.servers.3.url', 'http://localhost:8080/sabnzbd/');
 pref('extensions.nzbdstatus.servers.3.type', 'sabnzbd+');
 pref('extensions.nzbdstatus.servers.3.icon', 'yellow.png');
 
-pref('extensions.nzbdstatus.servers.4.url', 'http://dq5.no-ip.info:8080/sabnzbd/');
+pref('extensions.nzbdstatus.servers.4.enable', true);
+pref('extensions.nzbdstatus.servers.4.showNotifications', true);
+pref('extensions.nzbdstatus.servers.4.showInStatusBar', true);
 pref('extensions.nzbdstatus.servers.4.label', 'Test');
+pref('extensions.nzbdstatus.servers.4.url', 'http://dq5.no-ip.info:8080/sabnzbd/');
 pref('extensions.nzbdstatus.servers.4.type', 'sabnzbd+');
 pref('extensions.nzbdstatus.servers.4.icon', 'yellow.png');
