e75d3ea677cd71bf92e14c01e4d45641be030824
duz <duz@9d9971a0-073c-0410-b583-986eaf280b9e>
2010-02-24 19:26:45 +0000

    Add backup files to the exclusion list
    git-svn-id: https://nzbdstatus.svn.sourceforge.net/svnroot/nzbdstatus/trunk@317 9d9971a0-073c-0410-b583-986eaf280b9e

--- build-wip.sh
+++ build-wip.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-mv install-wip.rdf install.rdf
-7z a -tzip nzbdstatus.xpi * -r -mx=9 -xr\!.svn -x\!\*.bat -x\!\*.sh -x\!\*.xpi
-mv install.rdf install-wip.rdf
+7z a -tzip nzbdstatus.xpi * -r -mx=9 -xr\!.svn -x\!\*.bat -x\!\*.sh -x\!\*.xpi -x\!\*~
