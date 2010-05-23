0427f847075f503876a9f8e4cab5b97f85d929c3
duz <duz@9d9971a0-073c-0410-b583-986eaf280b9e>
2010-05-23 02:48:35 +0000

    Add sendUrl method Lots of little code tweaks to adjust to rewrite
    git-svn-id: https://nzbdstatus.svn.sourceforge.net/svnroot/nzbdstatus/trunk@376 9d9971a0-073c-0410-b583-986eaf280b9e

--- chrome/content/sabnzbdServerObject.js
+++ chrome/content/sabnzbdServerObject.js
@@ -34,8 +34,9 @@ function sabnzbdServerObject(serverId)
 	this.apikey = this.getPreference('apikey');
 	this.username = this.getPreference('username');
 	this.password = this.getPreference('password');
-	this.type = this.getPreference('type');
+	this.type = 'sabnzbd';
 	this.icon = this.getPreference('icon');
+	this.extension = 'nzb';
 
 	} catch(e) { this.errorLogger('connect',e); }
 }
@@ -49,6 +50,7 @@ sabnzbdServerObject.prototype = {
 
 	// Private properties
 	_preferences: null,
+	_queueHttp: new XMLHttpRequest(), // One httpRequest object for the processing queue
 
 
 	// Getters
@@ -63,6 +65,12 @@ sabnzbdServerObject.prototype = {
 	get xmlHttp()
 	{
 		return new XMLHttpRequest();
+	},
+
+	// Ajax for the processing queue
+	get queueHttp()
+	{
+		return this._queueHttp;
 	},
 
 
@@ -117,6 +125,9 @@ sabnzbdServerObject.prototype = {
 	},
 
 
+	///
+	/// Startup & init functions
+	///
 
 	// This is the stuff you do when you add the server
 	connect: function()
@@ -126,16 +137,6 @@ sabnzbdServerObject.prototype = {
 		this.checkIfApiKeyNeeded();
 
 		} catch(e) { this.errorLogger('connect',e); }
-
-/*			// There's no apikey stored so we'll check if there's a password saved
-			rootUrl = fullUrl.match(/(https?:\/\/([^\/]+))/)[1];
-			var logins = passwordManager.findLogins({}, rootUrl, rootUrl, null);
-			if (logins.length > 0)
-			{
-				username = logins[0].username;
-				password = logins[0].password;
-			}
-*/
 	},
 
 	checkIfApiKeyNeeded: function()
@@ -145,7 +146,7 @@ sabnzbdServerObject.prototype = {
 		var thisServer = this;
 		xmldoc.open('GET', fullUrl, true);
 		xmldoc.onload = function() { thisServer.checkIfApiKeyNeededResponse(this); };
-		this.timeout = setTimeout(function() { thisServer.abortTest(xmldoc); }, 10000);
+		this.connectiontimeout = setTimeout(function() { thisServer.abortTest(xmldoc); }, 30000);
 		xmldoc.send(null);
 	},
 
@@ -153,11 +154,14 @@ sabnzbdServerObject.prototype = {
 	{
 		xmldoc.abort();
 		this.connected = false;
+		this.logger('Timed out trying to connect to server for initial check');
 	},
 
 	checkIfApiKeyNeededResponse: function(that)
 	{
-		clearTimeout(this.timeout);
+		try {
+
+		clearTimeout(this.connectiontimeout);
 		if (that.status == 200)
 		{
 			if ((that.responseText.search('<title>Login</title>') > -1) || (that.responseText.search('Missing authentication') > -1))
@@ -166,6 +170,7 @@ sabnzbdServerObject.prototype = {
 				{
 					// The username/password we tried was wrong, stop trying for now
 					this.connected = false;
+					this.logger('Username/Password required but was wrong');
 					return;
 				}
 				// Username / Password required
@@ -189,7 +194,7 @@ sabnzbdServerObject.prototype = {
 			{
 				// Lets confirm we need the key and that it's correct
 				this.apikeyNeeded = true;
-				var fullUrl = this.url + 'api?mode=get_cats&output=xml&apikey=' + this.apikey;
+				var fullUrl = this.url + 'api?mode=get_cats&output=json&apikey=' + this.apikey;
 				if (this.loginNeeded)
 				{
 					fullUrl += '&ma_username='+this.username+'&ma_password='+this.password;
@@ -198,6 +203,7 @@ sabnzbdServerObject.prototype = {
 				var thisServer = this;
 				xmldoc.open('GET', fullUrl, true);
 				xmldoc.onload = function() { thisServer.confirmApiKeyNeeded(this); };
+				this.connectiontimeout = setTimeout(function() { thisServer.abortTest(xmldoc); }, 30000);
 				xmldoc.send(null);
 			}
 		}
@@ -208,10 +214,15 @@ sabnzbdServerObject.prototype = {
 			this.serverVersion = '0.4.6';
 		}
 		this.connected = true;
+
+		} catch(e) { this.errorLogger('checkIfApiKeyNeededResponse',e); }
 	},
 
 	confirmApiKeyNeeded: function(that)
 	{
+		try {
+
+		clearTimeout(this.connectiontimeout);
 		if ((that.status == 200) || (that.status == 500))
 		{
 			if (that.responseText.search('Missing authentication') > -1)
@@ -219,6 +230,8 @@ sabnzbdServerObject.prototype = {
 				if (this.loginNeeded)
 				{
 					// The username/password we tried was wrong, stop trying for now
+					this.connected = false;
+					this.logger('Username/Password required but was wrong');
 					return;
 				}
 				// Username / Password required
@@ -229,6 +242,7 @@ sabnzbdServerObject.prototype = {
 			}
 			if ((that.responseText.search('API Key Required') > -1) || (that.responseText.search('API Key Incorrect') > -1))
 			{
+				// Open a new tab with the config and prompt for the apikey
 				var newTabBrowser = gBrowser.getBrowserForTab(gBrowser.selectedTab = gBrowser.addTab(this.url + 'config/general/'));
 				newTabBrowser.addEventListener('load', function() {
 					var apikeydoc = newTabBrowser.contentDocument.getElementById('apikey');
@@ -251,6 +265,53 @@ sabnzbdServerObject.prototype = {
 				}
 			}
 		}
+
+		} catch(e) { this.errorLogger('confirmApiKeyNeeded',e); }
+	},
+
+
+	///
+	/// Send events
+	///
+
+	sendUrl: function(eventDetails)
+	{
+		try{
+
+nzbdStatus.logger('in sendurl');
+		var fullUrl = this.url + 'api?mode=addurl';
+		fullUrl += '&name='+encodeURIComponent(eventDetails.url);
+		if (this.loginNeeded)
+		{
+			fullUrl += '&ma_username='+this.username+'&ma_password='+this.password;
+		}
+		if (this.apikeyNeeded)
+		{
+			fullUrl += '&apikey='+this.apikey;
+		}
+		if ((eventDetails.category != null) && (eventDetails.category != 'None'))
+		{
+			fullUrl += '&cat='+eventDetails.category;
+		}
+nzbdStatus.logger(fullUrl);
+
+		var processingResponse = this.processingResponse;
+		var queueHttp = this.queueHttp;
+		queueHttp.open('GET', fullUrl, true);
+		queueHttp.onload = function() { processingResponse(this.responseText, eventDetails) };
+		queueHttp.send(null);
+
+		} catch(e) { this.errorLogger('sendUrl',e); }
+	},
+
+	processingResponse: function(responseText, eventDetails)
+	{
+		//
+	},
+
+	abortLastSend: function()
+	{
+		this.queueHttp.abort();
 	},
 
 	refresh: function()
@@ -309,7 +370,7 @@ sabnzbdServerObject.prototype = {
 		speed = queueData.kbpersec;
 		totalMbRemain = queueData.mbleft;
 		totalMb = queueData.mb;
-dump('1\n');
+//dump('1\n');
 		if (queueData.jobs.length > 0)
 		{
 			curDL = queueData.jobs[0].filename;
@@ -320,7 +381,7 @@ dump('1\n');
 		{
 			status = 'pause';
 		}
-dump('2\n');
+//dump('2\n');
 
 		if (status == 'pause')
 		{
@@ -372,7 +433,7 @@ dump('curTime: '+curTime+'\n')
 		{
 			return;
 		}
-		var msg = fName + ' threw error `' + error.message + '` on line: ' + error.lineNumber + '\n';
+		var msg = 'sabnzbdServerObject.' + fName + ' threw error `' + error.message + '` on line: ' + error.lineNumber + '\n';
 		switch (nzbdStatus.getPreference('errorMode'))
 		{
 			case 0: // alert
@@ -380,6 +441,27 @@ dump('curTime: '+curTime+'\n')
 				break;
 			case 1: // dump
 				dump(msg);
+				break;
+			case 2: // console.log
+				console.log(msg);
+				break;
+		}
+	},
+
+	// Outputting of log messages happens here
+	logger: function(msg)
+	{
+		if (!nzbdStatus.getPreference('showLogger'))
+		{
+			return;
+		}
+		switch (nzbdStatus.getPreference('loggerMode'))
+		{
+			case 0: // alert
+				alert(msg);
+				break;
+			case 1: // dump
+				dump(msg+'\n');
 				break;
 			case 2: // console.log
 				console.log(msg);
