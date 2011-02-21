a409bea8ed682d9710e09c928be5058283323a71
duz <duz@9d9971a0-073c-0410-b583-986eaf280b9e>
2011-02-21 22:20:42 +0000

    Don't include svg files
    git-svn-id: https://nzbdstatus.svn.sourceforge.net/svnroot/nzbdstatus/trunk@475 9d9971a0-073c-0410-b583-986eaf280b9e

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-7z a -tzip nzbdstatus.xpi * -r -mx=9 -xr\!.svn -x\!\*.bat -x\!\*.sh -x\!\*.xpi -x\!\*~
+7z a -tzip nzbdstatus.xpi * -r -mx=9 -xr\!.svn -x\!\*.bat -x\!\*.sh -x\!\*.xpi -x\!\*~ -x\!\*.svg
