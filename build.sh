60d436359146ee2598916163178d35d066967410
wolrahnaes <wolrahnaes@9d9971a0-073c-0410-b583-986eaf280b9e>
2012-05-04 20:32:53 +0000

    build.sh timestamps resulting XPI
    git-svn-id: https://nzbdstatus.svn.sourceforge.net/svnroot/nzbdstatus/trunk@487 9d9971a0-073c-0410-b583-986eaf280b9e

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-7z a -tzip nzbdstatus.xpi * -r -mx=9 -xr\!.git -x\!\*.bat -x\!\*.sh -x\!\*.xpi -x\!\*~ -x\!\*.svg
+7z a -tzip nzbdstatus-`date -u +%Y%M%d%H%M%S`.xpi * -r -mx=9 -xr\!.git -x\!\*.bat -x\!\*.sh -x\!\*.xpi -x\!\*~ -x\!\*.svg
